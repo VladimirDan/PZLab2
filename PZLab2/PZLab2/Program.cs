3de189beb232b01c08d66f95310d34419169b1ab
VladimirDan <vovaoil2003@gmail.com>
2023-03-26 14:46:26 +0300

    fix previous bug, added another to check if test fall

--- PZLab2/PZLab2/Program.cs
+++ PZLab2/PZLab2/Program.cs
@@ -55,7 +55,7 @@ namespace PZLab2
                 {
                     current = current.Next;
                 }
-                list.AddAfter(current, newNode);
+                list.AddAfter(current.Previous, newNode);
             }
         }
 
